32ad13e05be94a1136b8218f5f74a09ea44ac7df
hackademix <giorgio@maone.net>
2019-01-21 23:18:04 +0100

    Reduce touch (mobile) UI lag.

--- html/mobile.js
+++ html/mobile.js
@@ -1,1 +1,7 @@
 window.isMobile = !("windows" in browser);
+if (isMobile) {
+  let s = document.createElement("script");
+  s.src = "/html/fastclick.js";
+  document.head.appendChild(s);
+  window.addEventListener("DOMContentLoaded", e => FastClick.attach(document.body));
+}
